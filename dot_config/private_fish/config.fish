f22955d26180558d6ef7360a1e22e1a8836f24a8
Mario Lopez <mario@techmunchies.net>
2021-11-21 18:40:53 -0500

    Tweaked fish config, enabled starship prompt

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,3 +1,8 @@
+# Source custom configs
+for file in $__fish_config_dir/custom.d/*.fish
+    source $file
+end
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
@@ -5,3 +10,13 @@ end
 # User specific environment
 fish_add_path ~/.local/bin
 fish_add_path ~/bin
+
+# Launch Starship prompt
+if type -q starship
+    starship init fish | source
+end
+
+# Launch The Fuck
+if type -q thefuck
+    thefuck --alias | source
+end
