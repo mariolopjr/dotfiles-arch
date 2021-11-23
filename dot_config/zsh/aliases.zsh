3d7d4d1a6cd06e24b4b41787234e3263a87b57cf
Mario Lopez <mario@techmunchies.net>
2021-11-22 20:07:40 -0500

    Added zsh aliases and shell setup

---
+++ dot_config/zsh/aliases.zsh
@@ -0,0 +1,18 @@
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias less='less -R'
+
+# Add confirmation
+alias rm='rm -i'
+alias cp='cp -i'
+alias mv='mv -i'
+
+alias vi="nvim"
+alias vim="nvim"
+
+# Alias chezmoi commands
+alias cma="chezmoi add"
+alias cmay="chezmoi -v apply"
+alias cme="chezmoi edit"
+alias cmcd="chezmoi cd"
