ddf9e3dbfc88398fab2bbaafeb3fedcfa5f64752
Mario Lopez <mario@techmunchies.net>
2022-04-06 20:19:29 -0400

    Updated to fully script installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,10 +4,13 @@
 sudo rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 # Add global packages (creates a ostree layer)
-sudo rpm-ostree install akmod-nvidia xorg-x11-drv-nvidia steam python-pip virt-manager virt-viewer virt-top libvirt zsh vagrant neovim tmux bat fzf ripgrep fd-find mozilla-openh264
+sudo rpm-ostree install akmod-nvidia xorg-x11-drv-nvidia steam virt-manager virt-viewer virt-top libvirt zsh vagrant neovim tmux bat git-delta fzf ripgrep fd-find mozilla-openh264
 
 # Disallow nouveau driver from loading
 sudo rpm-ostree kargs --append=modprobe.blacklist=nouveau
 
 # Create default toolbox
 toolbox create
+
+# Init chezmoi
+$ sh -c "$(curl -fsLS chezmoi.io/get)" -- init --apply mariolopjr
