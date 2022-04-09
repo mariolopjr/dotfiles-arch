aeb4a602b479c5c27e53db90f226a836fc28a4dc
Mario Lopez <mario@techmunchies.net>
2022-04-09 08:20:00 -0400

    Swap from Fedora to Arch btw

--- README.md
+++ README.md
@@ -1,24 +1,5 @@
 # Mario's dotfiles repo
-My personal dotfile repo (in case you were wondering). Can completely bootstrap a Fedora Kinoite workstation with the included [kickstart](https://docs.fedoraproject.org/en-US/fedora/latest/install-guide/appendixes/Kickstart_Syntax_Reference/) files (currently, only my desktop workstation, but when I get a non-mac laptop that will be added as well). With the `bootstrap.sh` script, the system is bootstrapped with OS-level packages and then [chezmoi](https://www.chezmoi.io/) is initiliazed, getting my dotfiles in order.
+My personal dotfile repo (in case you were wondering). Can completely bootstrap an Arch Linux workstation (currently, only my desktop workstation, but when I get a non-mac laptop that will be added as well). With the `bootstrap.sh` script, the system is bootstrapped with packages and then [chezmoi](https://www.chezmoi.io/) is initiliazed, getting my dotfiles in order.
 
-### Kickstart system
-* Clone and enter this repository with (likely not necessary if using an existing system):
-  * `git clone https://github.com/mariolopjr/dotfiles`.
-  * `cd dotfiles`
-* You may want to change some hardcoded values if using the included kickstart:
-  * `sed -i 's/monolith/YOUR_HOSTNAME/' kickstarts/monolith.ks`
-  * `sed -i 's/mario/YOUR_ACCOUNT_NAME/' kickstarts/monolith.ks`
-  * `sed -i 's/Mario Lopez/YOUR_NAME/' kickstarts/monolith.ks`
-  * `sed -i 's/^timezone/timezone YourCountry\/YourCity/' kickstarts/monolith.ks`
-* Run the kickstart.go script which will launch an HTTP server:
-  * `go run .\kickstart.go -fdepass test -userpass test`
-* NOTE: at the moment, the kickstart will contain an unencrypted value for the user profile
-* Install Fedora Server netinst to a usb with `dd if=netinst.iso of=/dev/sdx bs=1M oflag=sync status=progress`
-* Boot Fedora Server netinst on the target install machine
-* At the boot screen, press UP to select the "install without verify" option and then 'e'
-* Insert the kickstart directive into the the boot string **before** the `quiet` directive:
-  * `inst.ks=http://<WEB_SERVER_IP>:<PORT>/workstation.ks`
-* Hit Ctrl-x. Install will begin and complete without any further prompt
-
-### Bootstrap system
-One the system reboots, unlock the FDE with the fde password supplied, then log in as with the user and password. Hit Ctrl+Alt+T to launch Konsole, then run this `bash <(curl -sL git.io/JMnGu)` to bootstrap the system and get dotfiles installed.
+### Bootstrap Arc Linux
+Ensure you have network connectivity, then run this `bash <(curl -sL git.io/JMnGu)` script to bootstrap the system and get dotfiles installed.
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,89 @@
-#!/bin/bash
+#!/usr/bin/env -S bash -e
 
-# Tweak fstab btrfs mount options
+# Cleaning the TTY.
+clear
 
-# Add non-free repo
-sudo rpm-ostree install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+# Pretty print (function).
+print () {
+    echo -e "\e[1m\e[93m[ \e[92m•\e[93m ] \e[4m$1\e[0m"
+}
 
-# Add global packages (creates an ostree layer)
-sudo rpm-ostree install akmod-nvidia xorg-x11-drv-nvidia steam virt-manager virt-viewer virt-top libvirt kitty zsh vagrant neovim tmux bat git-delta fzf ripgrep fd-find mozilla-openh264
+get_username () {
+    read -r -p "Please enter name for a user account: " username
+    if [ -z "$username" ]; then
+        print "You need to enter a valid username in order to continue."
+        get_username
+    fi
+}
 
-# Disallow nouveau driver from loading
-sudo rpm-ostree kargs --append=modprobe.blacklist=nouveau
+# Prepare system for install
+bash <(curl -sL git.io/JMnfF)
 
-# Create default toolbox
-toolbox create
+# Enable multilib repo
+sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
+sed -i "/\[multilib\]/,/Include/"'s/^#//' /mnt/etc/pacman.conf
 
-# Init chezmoi
-$ sh -c "$(curl -fsLS chezmoi.io/get)" -- init --apply mariolopjr
+get_username
+
+# Install NVIDIA proprietary driver if necessary
+read -r -p "Do you want to install nvidia proprietary driver? [y/N]? " response
+response=${response,,}
+if [[ "$response" =~ ^(yes|y)$ ]]; then
+    print "Installing nvidia driver."
+    pacstrap /mnt nvidia nvidia-utils
+    echo "MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)" >> /mnt/etc/mkinitcpio.conf
+
+    print "Rebuilding initramfs"
+    arch-chroot /mnt mkinitcpio -P &>/dev/null
+    
+    print "Configuring pacman initramfs rebuild on nvidia driver update"
+    cat > /mnt/etc/pacman.d/hooks/60-nvidia.hook <<EOF
+[Trigger]
+Operation=Install
+Operation=Upgrade
+Operation=Remove
+Type=Package
+Target=nvidia
+Target=linux
+# Change the linux part above and in the Exec line if a different kernel is used
+[Action]
+Description=Update Nvidia module in initcpio
+Depends=mkinitcpio
+When=PostTransaction
+NeedsTargets
+Exec=/bin/sh -c 'while read -r trg; do case $trg in linux) exit 0; esac; done; /usr/bin/mkinitcpio -P'
+EOF
+fi
+
+# Install packages
+pacstrap /mnt \
+	kitty kitty-shell-integration kitty-terminfo zsh starship \
+	neovim bat ripgrep exa fd wget fzf unzip zip dialog ddcutil \
+	pacman-contrib bat ncdu pv zsh-completions watchexec tmux xclip \
+	lsof bind-tools mtr socat htop iotop openbsd-netcat strace whois \
+	e2fsprogs exfat-utils dosfstools f2fs-tools ddrescue \
+	git git-delta jq ddrescue  \
+	podman podman-dnsname buildah dnsmasq \
+	qemu qemu-arch-extra virt-manager vagrant \
+	clang go nodejs \
+	plasma-desktop plasma-wayland-session kde-system-meta \
+	kdeconnect kdenetwork-filesharing kget kio-extras kio-gdrive krdc krfb ktorrent zeroconf-ioslave \
+	kleopatra kcmutils dolphin-plugins kdeplasma-addons \
+    ark filelight kcalc kcharselect kdebugsettings kdf kdialog kfind kgpg kwrite kdiff markdownpart print-manager \
+	quota-tools sddm rng-tools \
+	firefox discord guitarix bitwarden bitwarden-cli \
+	redshift pipewire scrot arandr x264 x265 \
+	steam mgba-qt ppsspp pcsx2
+
+# Install AUR packages
+arch-chroot /mnt sudo -H -u "$username" bash -c "
+	sudo systemctl enable sshd
+	rm -rf /home/$username/paru
+
+    paru -S --noconfirm opensnitch bitwig-studio citra-git rpcs3-git bsnes-qt5 nerd-fonts-fira-code protonup-git protonup-qt plymouth-git plymouth-theme-arch-logo-new refind-theme-nord nordic-kde-git kvantum-theme-nordic-git nordic-theme-git sddm-nordic-theme-git macchina
+
+	sh -c "$(curl -fsLS chezmoi.io/get)" -- init --apply mariolopjr
+"
+
+# Enable SDDM
+systemctl enable sddm --root=/mnt &>/dev/null
